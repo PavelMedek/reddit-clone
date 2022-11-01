60b74a04eda7faf599b72692bd195272f79331c8
PavelMedek <medek41@gmail.com>
2022-11-01 14:58:59 +0100

    first commit

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,77 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React, { useEffect } from "react";
+import { Community, communityState } from "../../../atoms/communitiesAtom";
+import { firestore } from "../../../firebase/clientApp";
+import safeJsonStringify from "safe-json-stringify";
+import CommunityNotFound from "../../../components/Community/NotFound";
+import Header from "../../../components/Community/Header";
+import PageContent from "../../../components/Layout/PageContent";
+import CreatePostLink from "../../../components/Community/CreatePostLink";
+import Posts from "../../../components/Posts/Posts";
+import { useSetRecoilState } from "recoil";
+import About from "../../../components/Community/About";
+
+type CommunityPageProps = {
+  communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  console.log("here is data", communityData);
+  const setCommunityStateValue = useSetRecoilState(communityState);
+
+  useEffect(() => {
+    setCommunityStateValue((prev) => ({
+      ...prev,
+      currentCommunity: communityData,
+    }));
+  }, [communityData]);
+
+  if (!communityData) {
+    return <CommunityNotFound />;
+  }
+
+  return (
+    <>
+      <Header communityData={communityData} />
+      <PageContent>
+        <>
+          <div>
+            <CreatePostLink />
+            <Posts communityData={communityData} />
+          </div>
+        </>
+        <>
+          <About communityData={communityData} />
+        </>
+      </PageContent>
+    </>
+  );
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
+      },
+    };
+  } catch (error) {
+    console.log("getServerSideProps error", error);
+  }
+}
+
+export default CommunityPage;
